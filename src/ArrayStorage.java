61bf4471e4f7fb72316f1e7508ecd8a7ec08fc35
kobts <kobts@yandex.ru>
2023-05-22 22:54:52 +0400

    HW1

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -5,26 +5,50 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
     void clear() {
+        for (int i = 0; i < storage.length && storage[i] != null; i++) {
+            storage[i] = null;
+        }
     }
 
     void save(Resume r) {
+        storage[size()] = r;
     }
 
     Resume get(String uuid) {
+        for (int i = 0; i < storage.length && storage[i] != null; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                return storage[i];
+            }
+        }
         return null;
     }
 
     void delete(String uuid) {
+        int n = 0;
+        for (int i = 0; i < storage.length && storage[i] != null; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                n = i;
+            }
+            if (i >= n) {
+                storage[i] = storage[i + 1];
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        Resume[] resumes = new Resume[size()];
+        System.arraycopy(storage, 0, resumes, 0, resumes.length);
+        return resumes;
     }
 
     int size() {
-        return 0;
+        int size = 0;
+        for (int i = 0; i < storage.length && storage[i] != null; i++) {
+            size++;
+        }
+        return size;
     }
 }
