5e829950fbb611c78502f1ae9d4767943e89ee69
kobts <kobts@yandex.ru>
2023-05-31 23:26:03 +0400

    Refactoring HW1

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,21 +1,25 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    private int size = 0;
 
     void clear() {
-        for (int i = 0; i < storage.length && storage[i] != null; i++) {
+        for (int i = 0; i < size; i++) {
             storage[i] = null;
         }
     }
 
     void save(Resume r) {
-        storage[size()] = r;
+        storage[size] = r;
+        size++;
     }
 
     Resume get(String uuid) {
-        for (int i = 0; i < storage.length && storage[i] != null; i++) {
+        for (int i = 0; i < size; i++) {
             if (storage[i].uuid.equals(uuid)) {
                 return storage[i];
             }
@@ -24,12 +28,12 @@ public class ArrayStorage {
     }
 
     void delete(String uuid) {
-        int n = 0;
-        for (int i = 0; i < storage.length && storage[i] != null; i++) {
+        int foundItemIndex = 0;
+        for (int i = 0; i < size; i++) {
             if (storage[i].uuid.equals(uuid)) {
-                n = i;
+                foundItemIndex = i;
             }
-            if (i >= n) {
+            if (i >= foundItemIndex) {
                 storage[i] = storage[i + 1];
             }
         }
@@ -39,16 +43,10 @@ public class ArrayStorage {
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        Resume[] resumes = new Resume[size()];
-        System.arraycopy(storage, 0, resumes, 0, resumes.length);
-        return resumes;
+        return Arrays.copyOf(storage, size);
     }
 
     int size() {
-        int size = 0;
-        for (int i = 0; i < storage.length && storage[i] != null; i++) {
-            size++;
-        }
         return size;
     }
 }
