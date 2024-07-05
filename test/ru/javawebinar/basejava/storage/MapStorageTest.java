a47ffb310960740845412b32b6b1d6d3012732f3
kobts <kobts@yandex.ru>
2024-07-05 09:56:28 +0400

    HW5

---
+++ test/ru/javawebinar/basejava/storage/MapStorageTest.java
@@ -0,0 +1,14 @@
+package ru.javawebinar.basejava.storage;
+
+import org.junit.Test;
+
+public class MapStorageTest extends AbstractStorageTest {
+
+    public MapStorageTest() {
+        super(new MapStorage());
+    }
+
+    @Test()
+    public void saveOverflow() {
+    }
+}
